9dbca533733b5dad2d25e799354c48e5441635c7
Punpun <jersonraydesierto@gmail.com>
2024-08-17 10:46:48 +0800

    chore: fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.jerson.hcdc_portal"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.1"
 
         val prop = Properties()
